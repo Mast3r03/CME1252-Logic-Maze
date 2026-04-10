398641eef8b36a88aa025b1fcb6200ceddc73951
Can Özdemir <c.ozdemir@ogr.deu.edu.tr>
2026-04-10 11:37:11 +0300

    player'a backpack yönetimi eklendi

--- src/Player.java
+++ src/Player.java
@@ -1,11 +1,15 @@
 public class Player {
+    private static final int BACKPACK_CAPACITY = 8;
+
     private int col;
     private int row;
     private int moveCooldown = 0;
+    private Stack backpack;
 
     public Player(int col, int row) {
         this.col = col;
         this.row = row;
+        this.backpack = new Stack(BACKPACK_CAPACITY);
     }
 
     public int getCol() { return col; }
@@ -33,5 +37,37 @@ public class Player {
         this.col = this.col + dx;
         this.row = this.row + dy;
         this.moveCooldown = GameConstants.PLAYER_MOVE_INTERVAL;
+    }
+
+    public int getBackpackCapacity() {
+        return backpack.capacity();
+    }
+
+    public int getBackpackCount() {
+        return backpack.size();
+    }
+
+    public boolean isBackpackEmpty() {
+        return backpack.isEmpty();
+    }
+
+    public boolean isBackpackFull() {
+        return backpack.isFull();
+    }
+
+    public boolean pushBackpack(char item) {
+        return backpack.push(item);
+    }
+
+    public char popBackpack() {
+        return backpack.pop();
+    }
+
+    public char peekBackpack() {
+        return backpack.peek();
+    }
+
+    public char[] getBackpackSnapshot() {
+        return backpack.snapshot();
     }
 }
